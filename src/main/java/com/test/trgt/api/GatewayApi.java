ab33e737a3ee78ddf85ce4bc3f7d0a0e10f7d845
hobbes09 <sourin.sutradhar@gmail.com>
2017-06-14 19:05:49 +0530

    gateway API

---
+++ src/main/java/com/test/trgt/api/GatewayApi.java
@@ -0,0 +1,38 @@
+package com.test.trgt.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.test.trgt.models.Product;
+import com.test.trgt.service.ProductService;
+
+@Path("/api")
+public class GatewayApi {
+	
+	ProductService productService;
+	
+	public GatewayApi() {
+		super();
+		productService = new ProductService();
+	}
+
+	@GET
+	@Path("/")
+	public Response resp(){
+		return Response.status(200).entity("HOME PATH !").build();
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/products/{id}")
+	public Product responseMsg(@PathParam("id") long productId) {
+
+		Product product = productService.getProductDetailsById(productId);
+				
+		return product;
+	}
+}
