2a139a0c350e5ed6b60ad02c3dba5ab6d8d64787
hobbes09 <sourin.sutradhar@gmail.com>
2017-06-14 19:06:01 +0530

    product model

---
+++ src/main/java/com/test/trgt/config/ApplicationConfig.java
@@ -0,0 +1,5 @@
+package com.test.trgt.config;
+
+public class ApplicationConfig {
+
+}
---
+++ src/main/java/com/test/trgt/models/Product.java
@@ -0,0 +1,25 @@
+package com.test.trgt.models;
+
+public class Product {
+	
+	private long id;
+	private String name;
+	
+	public Product(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
