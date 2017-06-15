b81938b48db30049ca5778480670ad091b7e25e1
hobbes09 <sourin.sutradhar@gmail.com>
2017-06-15 15:53:10 +0530

    models added

---
+++ src/main/java/com/test/trgt/models/Error.java
@@ -0,0 +1,30 @@
+package com.test.trgt.models;
+
+public class Error {
+	
+	public String message;
+	public int code;
+
+	public Error(String message, int code) {
+		super();
+		this.message = message;
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+}
--- src/main/java/com/test/trgt/models/Product.java
+++ src/main/java/com/test/trgt/models/Product.java
@@ -4,11 +4,21 @@ public class Product {
 	
 	private long id;
 	private String name;
+	private String url;
+	private Error error;
 	
-	public Product(long id, String name) {
+	public Product(){
+		this.id = 0;
+		this.name = "";
+		this.url = "";
+	}
+	
+	public Product(long id, String name, String url, Error error) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.url = url;
+		this.error = error;
 	}
 	public long getId() {
 		return id;
@@ -22,4 +32,17 @@ public class Product {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public Error getError() {
+		return error;
+	}
+	public void setError(Error error) {
+		this.error = error;
+	}
+	
 }
