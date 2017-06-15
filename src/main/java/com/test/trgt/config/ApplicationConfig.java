accd7b5b0b5c60fb6315a4010b5573008756282e
hobbes09 <sourin.sutradhar@gmail.com>
2017-06-15 15:52:55 +0530

    application configs and constants

--- src/main/java/com/test/trgt/config/ApplicationConfig.java
+++ src/main/java/com/test/trgt/config/ApplicationConfig.java
@@ -1,5 +1,15 @@
 package com.test.trgt.config;
 
 public class ApplicationConfig {
+	
+	public static final String EXT_BASE_URL = "api.target.com";
+	
+	public static final String SCHEME = "https";
+	
+	public static final String EXT_FIELDS = "descriptions";
+	
+	public static final String EXT_ID_TYPE = "TCIN";
+	
+	public static final String EXT_KEY = "43cJWpLjH8Z8oR18KdrZDBKAgLLQKJjz";
 
 }
